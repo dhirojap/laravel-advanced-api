bbd05e649f8c59b9e82b0c7b1a22fcb5945c94a6
DhiroJap <dhirojap@gmail.com>
2024-03-17 02:22:18 +0700

    Create Middleware for Routing and Route for Get User

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthApiMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::post("/auth/register", [UserController::class, "register"]);
 Route::post("/auth/login", [UserController::class, "login"]);
+
+Route::middleware(AuthApiMiddleware::class)->group(function () {
+    Route::get("/auth/users/current", [UserController::class,"getUser"]);
+});
