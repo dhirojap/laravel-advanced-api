026f578dddb0767c08ff5eb28f40218715a47e6d
DhiroJap <dhirojap@gmail.com>
2024-03-17 01:53:25 +0700

    Create User Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,10 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            "username" => "dhirojap",
+            "password" => "test",
+            "name" => "Dhiro Jap",
+        ]);
     }
 }
