aa28bc2f1f156cc8f5c8e4fa390bab5b9244553a
DhiroJap <dhirojap@gmail.com>
2024-03-17 01:50:58 +0700

    Create Login Route

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post("/auth/register", [UserController::class, "register"]);
+Route::post("/auth/login", [UserController::class, "login"]);
